54d0c2e31291321c2d546d2375a22e5413b6cdb9
chirag04 <jain_chirag04@yahoo.com>
2015-09-14 11:12:04 -0400

    add android support

---
+++ android/src/main/java/com/chirag/RNMail/RNMailModule.java
@@ -0,0 +1,63 @@
+package com.chirag.RNMail;
+
+import android.content.Intent;
+import android.content.ActivityNotFoundException;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.Callback;
+
+/**
+ * {@link NativeModule} that allows JS to open emails sending apps chooser.
+ */
+
+public class RNMailModule extends ReactContextBaseJavaModule {
+
+  ReactApplicationContext reactContext;
+
+  public RNMailModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNMail";
+  }
+
+  @ReactMethod
+  public void mail(ReadableMap options, Callback callback) {
+    Intent i = new Intent(Intent.ACTION_SEND);
+    i.setType("message/rfc822");
+
+    if (options.hasKey("subject") && !options.isNull("subject")) {
+      i.putExtra(Intent.EXTRA_SUBJECT, options.getString("subject"));
+    }
+
+    if (options.hasKey("body") && !options.isNull("body")) {
+      i.putExtra(Intent.EXTRA_TEXT, options.getString("body"));
+    }
+
+    if (options.hasKey("recipients") && !options.isNull("recipients")) {
+      ReadableArray r = options.getArray("recipients");
+      int length = r.size();
+      String[] recipients = new String[length];
+      for (int keyIndex = 0; keyIndex < length; keyIndex++) {
+        recipients[keyIndex] = r.getString(keyIndex);
+      }
+      i.putExtra(Intent.EXTRA_EMAIL, recipients);
+    }
+
+    Intent chooser = Intent.createChooser(i, "Send Mail");
+    chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    try {
+      reactContext.startActivity(chooser);
+    } catch (ActivityNotFoundException ex) {
+      callback.invoke("not_available");
+    }
+  }
+}
