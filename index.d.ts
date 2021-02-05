594f6c86effa957506daf42030c5159dc26d9369
jiggag <jiggag90@gmail.com>
2021-02-05 18:05:57 +0900

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+export namespace Mailer {
+  function mail(options: {
+    subject?: string;
+    recipients?: string[];
+    ccRecipients?: string[];
+    bccRecipients?: string[];
+    body?: string;
+    customChooserTitle?: string;
+    isHTML?: boolean;
+    attachments: {
+      path: string;
+      type?: string;
+      mimeType?: string;
+      name?: string;
+    }[]
+  }, callback: (
+    error: string,
+    event?: string
+  ) => void): void;
+}
+
+export default Mailer;
