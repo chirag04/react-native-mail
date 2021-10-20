43dd21d95b48352336f409e404a23aa5e9fa9429
Jinesh Francis <jineshfofficial@gmail.com>
2021-10-20 11:16:50 +0530

    Added success callback

--- android/src/main/java/com/chirag/RNMail/RNMailModule.java
+++ android/src/main/java/com/chirag/RNMail/RNMailModule.java
@@ -136,7 +136,7 @@ public class RNMailModule extends ReactContextBaseJavaModule {
       i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
       try {
         reactContext.startActivity(i);
-        callback.invoke();
+        callback.invoke("success");
       } catch (Exception ex) {
         callback.invoke("error");
       }
@@ -152,7 +152,7 @@ public class RNMailModule extends ReactContextBaseJavaModule {
 
       try {
         reactContext.startActivity(chooser);
-        callback.invoke();
+        callback.invoke("success");
       } catch (Exception ex) {
         callback.invoke("error");
       }
